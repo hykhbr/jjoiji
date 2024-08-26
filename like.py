5f30a974a1376b1f6679a4607a9705c2b140885c
hykhbr <149931352+hykhbr@users.noreply.github.com>
2024-08-26 19:43:02 +0800

    Add files via upload

---
+++ like.py
@@ -0,0 +1,33 @@
+while 1:
+
+    a_521=input("很高兴你能打开这个app，请先告诉我你的名字:(输入名字首字母哦)")
+
+    if a_521.upper()=="HBR":
+
+        print("星星会说话，石头会开花，穿过夏天的木栅栏和冬天的风雪之后，你终会抵达",a_521)
+
+        continue
+
+
+
+    else:
+
+        print("很高兴认你",a_521)
+
+        break
+
+A01=int(input('告诉我你的年龄,好嘛'))
+
+A02=input('可以透露下你是小哥哥还是小姐姐嘛')
+
+if int(A01) <=int('23') and A02=='小姐姐':
+
+    print('加QQ:xxxxxxxxxxxx，细说')
+
+    pass
+
+if int(A01)>=100:
+
+    B01 = int(input('请问“5+（99/33）”等于多少'))
+
+    print(666)
